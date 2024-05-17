9074ff0ea42a8ca144ba912a20fdf9760bb75538
Peerapat Mapitak <146796863+toffly@users.noreply.github.com>
2024-05-18 00:22:05 +0700

    Update config.js
    For config.js please contact me

--- Backend/config.js
+++ Backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 3000;
 
 export const mongoDBURL =
-'mongodb+srv://root:KL0RZDupsYgNTxhZ@bookstore.dz0u09m.mongodb.net/?retryWrites=true&w=majority&appName=Bookstore'
+'########'
